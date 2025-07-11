4473cae592ff4a3363a25fdcde72e008352ca596
gearlessjay <108426024+gearlessjay@users.noreply.github.com>
2025-07-11 12:02:53 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/yu-ryan/pen/pvjoGaG](https://codepen.io/yu-ryan/pen/pvjoGaG).
+
---
+++ src/script.js
@@ -0,0 +1,128 @@
+async function setup() {
+    const patchExportURL = "https://raw.githubusercontent.com/gearlessjay/rnbo/refs/heads/main/patch.export.json?token=GHSAT0AAAAAADHFHHMILJ26QWULC2OCLHZ62DQRQ7Q";
+
+    // Create AudioContext
+    const WAContext = window.AudioContext || window.webkitAudioContext;
+    const context = new WAContext();
+
+    // Create gain node and connect it to audio output
+    const outputNode = context.createGain();
+    outputNode.connect(context.destination);
+    
+    // Fetch the exported patcher
+    let response, patcher;
+    try {
+        response = await fetch(patchExportURL);
+        patcher = await response.json();
+    
+        if (!window.RNBO) {
+            // Load RNBO script dynamically
+            // Note that you can skip this by knowing the RNBO version of your patch
+            // beforehand and just include it using a <script> tag
+            await loadRNBOScript(patcher.desc.meta.rnboversion);
+        }
+
+    } catch (err) {
+        const errorContext = {
+            error: err
+        };
+        if (response && (response.status >= 300 || response.status < 200)) {
+            errorContext.header = `Couldn't load patcher export bundle`,
+            errorContext.description = `Check app.js to see what file it's trying to load. Currently it's` +
+            ` trying to load "${patchExportURL}". If that doesn't` + 
+            ` match the name of the file you exported from RNBO, modify` + 
+            ` patchExportURL in app.js.`;
+        }
+        if (typeof guardrails === "function") {
+            guardrails(errorContext);
+        } else {
+            throw err;
+        }
+        return;
+    }
+    
+    // (Optional) Fetch the dependencies
+    let dependencies = [];
+    try {
+        const dependenciesResponse = await fetch("export/dependencies.json");
+        dependencies = await dependenciesResponse.json();
+
+        // Prepend "export" to any file dependenciies
+        dependencies = dependencies.map(d => d.file ? Object.assign({}, d, { file: "export/" + d.file }) : d);
+    } catch (e) {}
+
+    // Create the device
+    let device;
+    try {
+        device = await RNBO.createDevice({ context, patcher });
+    } catch (err) {
+        if (typeof guardrails === "function") {
+            guardrails({ error: err });
+        } else {
+            throw err;
+        }
+        return;
+    }
+
+    // (Optional) Load the samples
+    if (dependencies.length)
+        await device.loadDataBufferDependencies(dependencies);
+
+    // Connect the device to the web audio graph
+    device.node.connect(outputNode);
+
+    document.body.onclick = () => {
+        context.resume();
+    }
+
+    // Skip if you're not using guardrails.js
+    if (typeof guardrails === "function")
+        guardrails();
+
+    document.getElementById("button").onclick = () => {
+
+    let note = Math.round(Math.random() * 127)
+
+    let midiNoteOn = [
+        144,
+        note,
+        127
+    ];
+
+    let midiNoteOff = [
+        128,
+        note,
+        0
+    ];
+
+    let midiPort = 0;
+    let midiOnEvent = new RNBO.MIDIEvent(context.currentTime * 1000, midiPort, midiNoteOn);
+    let midiOffEvent = new RNBO.MIDIEvent(context.currentTime * 1000 + 1000, midiPort, midiNoteOff);
+
+    device.scheduleEvent(midiOnEvent);
+    device.scheduleEvent(midiOffEvent);
+    };
+
+    document.getElementById("cutoff").oninput = (e) => {
+    let cutoff = e.target.value;
+    let param = device.parametersById.get("cutoff");
+    param.normalizedValue = cutoff;
+    };
+}
+function loadRNBOScript(version) {
+    return new Promise((resolve, reject) => {
+        if (/^\d+\.\d+\.\d+-dev$/.test(version)) {
+            throw new Error("Patcher exported with a Debug Version!\nPlease specify the correct RNBO version to use in the code.");
+        }
+        const el = document.createElement("script");
+        el.src = "https://c74-public.nyc3.digitaloceanspaces.com/rnbo/" + encodeURIComponent(version) + "/rnbo.min.js";
+        el.onload = resolve;
+        el.onerror = function(err) {
+            console.log(err);
+            reject(new Error("Failed to load rnbo.js v" + version));
+        };
+        document.body.append(el);
+    });
+}
+
+setup();
